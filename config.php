7618734a6666a8ef5a3c90f106736442d6fd44ae
hanyi <hanyi8335@163.com>
2026-06-05 11:35:50 +0800

    init

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    /*数据库配置*/
+    $dbconfig=array(
+        'host' => 'localhost', //数据库服务器
+        'port' => 3306, //数据库端口
+        'user' => 'wx_iiilh_com', //数据库用户名
+        'pwd' => 'YWid5GTTiQ544jiZ', //数据库密码
+        'dbname' => 'wx_iiilh_com', //数据库名
+        'dbqz' => 'pay' //数据表前缀
+    );
+    
